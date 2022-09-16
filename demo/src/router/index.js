d436032b0e9157ae86ca359c38dfa25f8604dd63
zm <1138195675@qq.com>
2022-09-16 20:28:23 +0800

    add leju_shop_admin

---
+++ demo/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ demo/src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="box">
+    <h1>Login Form</h1>
+    <p>
+      <el-input
+        placeholder="请输入用户名"
+        v-model="form.name"
+        prefix-icon="el-icon-user-solid"
+        style="width: 300px"
+      >
+      </el-input>
+    </p>
+    <p>
+      <el-input
+        placeholder="请输入密码"
+        v-model="form.password"
+        prefix-icon="el-icon-goods"
+        suffix-icon="el-icon-more"
+        style="width: 300px"
+      >
+      </el-input>
+    </p>
+    <el-button type="primary" style="width: 300px" @click="gotoHomepage"
+      >login</el-button
+    >
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      // 表单
+      form: {
+        name: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    // 登录
+    gotoHomepage() {
+      if (this.form.name == "admin" || this.form.password == "123456") {
+        localStorage.setItem("token",this.form)
+        this.$router.push("/");
+      } else {
+        alert("请输入正确的账号");
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.box {
+  width: 100%;
+  height: 100vh;
+  background-color: #2d3a4b;
+  color: #fff;
+  text-align: center;
+  padding-top: 200px;
+
+  p {
+    margin: 20px;
+  }
+  ::v-deep .el-input__inner {
+    background-color: #2d3a4b;
+  }
+}
+</style>
